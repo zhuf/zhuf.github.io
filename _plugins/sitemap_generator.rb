d518ca899f25f9a94fdce0872452fe071957deaa
zhuf <zhuf@live.com>
2013-06-07 18:41:25 +0800

    1

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://onox.com.br"
+  MY_URL = "http://zhuf.github.io"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
